8a5e7f42ded28c59dd5d44c5e9a820a107cdff6b
bonfiir <bonfiirco@gmail.com>
2026-03-17 11:32:33 -0400

    Add src/components/index.ts exports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { MainComposition } from './MainComposition';
+export { TitleSection } from './TitleSection';
+export { ProductShowcase } from './ProductShowcase';
+export { FeatureHighlights } from './FeatureHighlights';
+export { CallToAction } from './CallToAction';
+export { ParticleBackground } from './ParticleBackground';
